21d1abfdab63da04047b138583bf83216ba9f487
amirakbr <88962160+amirakbr@users.noreply.github.com>
2022-02-20 10:34:34 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+const searchbutton = document.querySelector('.fa-search') ; 
+searchbutton.addEventListener('click' , function(){
+   document.querySelector('.search').classList.toggle('hidden')
+}) ;
